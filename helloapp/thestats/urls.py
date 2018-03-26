89165f70d073e9b66388e93c97b0eeee2d148c7e
Dennis Nderitu <dknderitu@gmail.com>
2018-03-26 12:21:23 +0300

    first commit

---
+++ helloapp/thestats/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from thestats import views
+
+urlpatterns = [
+    url(r'^$', views.HomePageView.as_view()),
+    url(r'^about/$', views.AboutPageView.as_view()), # Add this /about/ route
+]
